1d92a7b7a9081179082ba304fd30031bb7a9c6a3
Jarrod Millman <millman@berkeley.edu>
2009-10-25 07:45:11 +0000

    don't want indices for main website

--- source/conf.py
+++ source/conf.py
@@ -139,9 +139,11 @@ html_static_path = ['_static']
 
 # If false, no module index is generated.
 #html_use_modindex = True
+html_use_modindex = False
 
 # If false, no index is generated.
 #html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
