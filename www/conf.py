7e03902c4fcea87a1d5959f832a3e7bf2b598b84
Jarrod Millman <millman@berkeley.edu>
2010-01-14 04:56:15 +0000

    cleaning up

--- www/conf.py
+++ www/conf.py
@@ -123,7 +123,7 @@ html_theme = 'scipy'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../theme"]
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
