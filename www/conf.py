ae24a15b8c6801962a47e83cb5e9361b8542d542
Jarrod Millman <millman@berkeley.edu>
2010-01-14 08:54:53 +0000

    reorganizing theme layout

--- www/conf.py
+++ www/conf.py
@@ -124,6 +124,7 @@ html_theme = 'scipy'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = ['_theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -145,7 +146,7 @@ html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_theme/scipy/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
