cc43e20399245316e9f798d76be7048ad853ddb0
Ralf Gommers <ralf.gommers@gmail.com>
2018-04-28 12:55:19 -0700

    MAINT: remove build warnings, clean up outdated Sphinx extensions
    There are only 2 build warnings left, those come from within the
    Matplotlib extension so cannot be removed here.

--- www/sphinxext/ipython_console_highlighting.py
+++ www/sphinxext/ipython_console_highlighting.py
@@ -1,98 +1,31 @@
-"""reST directive for syntax-highlighting ipython interactive sessions.
+"""
+reST directive for syntax-highlighting ipython interactive sessions.
+
+Original at https://github.com/ipython/ipython/blob/master/IPython/sphinxext/ipython_console_highlighting.py
+
 """
 
-#-----------------------------------------------------------------------------
-# Needed modules
-
-# Standard library
-import re
-
-# Third party
-from pygments.lexer import Lexer, do_insertions
-from pygments.lexers.agile import (PythonConsoleLexer, PythonLexer, 
-                                   PythonTracebackLexer)
-from pygments.token import Comment, Generic
-
 from sphinx import highlighting
+from IPython.lib.lexers import IPyLexer
 
+def setup(app):
+    """Setup as a sphinx extension."""
 
-#-----------------------------------------------------------------------------
-# Global constants
-line_re = re.compile('.*?\n')
+    # This is only a lexer, so adding it below to pygments appears sufficient.
+    # But if somebody knows what the right API usage should be to do that via
+    # sphinx, by all means fix it here.  At least having this setup.py
+    # suppresses the sphinx warning we'd get without it.
+    metadata = {'parallel_read_safe': True, 'parallel_write_safe': True}
+    return metadata
 
-#-----------------------------------------------------------------------------
-# Code begins - classes and functions
+# Register the extension as a valid pygments lexer.
+# Alternatively, we could register the lexer with pygments instead. This would
+# require using setuptools entrypoints: http://pygments.org/docs/plugins
 
-class IPythonConsoleLexer(Lexer):
-    """
-    For IPython console output or doctests, such as:
+ipy2 = IPyLexer(python3=False)
+ipy3 = IPyLexer(python3=True)
 
-    .. sourcecode:: ipython
+highlighting.lexers['ipython'] = ipy2
+highlighting.lexers['ipython2'] = ipy2
+highlighting.lexers['ipython3'] = ipy3
 
-      In [1]: a = 'foo'
-
-      In [2]: a
-      Out[2]: 'foo'
-
-      In [3]: print a
-      foo
-
-      In [4]: 1 / 0
-
-    Notes:
-
-      - Tracebacks are not currently supported.
-
-      - It assumes the default IPython prompts, not customized ones.
-    """
-    
-    name = 'IPython console session'
-    aliases = ['ipython']
-    mimetypes = ['text/x-ipython-console']
-    input_prompt = re.compile("(In \[[0-9]+\]: )|(   \.\.\.+:)")
-    output_prompt = re.compile("(Out\[[0-9]+\]: )|(   \.\.\.+:)")
-    continue_prompt = re.compile("   \.\.\.+:")
-    tb_start = re.compile("\-+")
-
-    def get_tokens_unprocessed(self, text):
-        pylexer = PythonLexer(**self.options)
-        tblexer = PythonTracebackLexer(**self.options)
-
-        curcode = ''
-        insertions = []
-        for match in line_re.finditer(text):
-            line = match.group()
-            input_prompt = self.input_prompt.match(line)
-            continue_prompt = self.continue_prompt.match(line.rstrip())
-            output_prompt = self.output_prompt.match(line)
-            if line.startswith("#"):
-                insertions.append((len(curcode),
-                                   [(0, Comment, line)]))
-            elif input_prompt is not None:
-                insertions.append((len(curcode),
-                                   [(0, Generic.Prompt, input_prompt.group())]))
-                curcode += line[input_prompt.end():]
-            elif continue_prompt is not None:
-                insertions.append((len(curcode),
-                                   [(0, Generic.Prompt, continue_prompt.group())]))
-                curcode += line[continue_prompt.end():]
-            elif output_prompt is not None:
-                insertions.append((len(curcode),
-                                   [(0, Generic.Output, output_prompt.group())]))
-                curcode += line[output_prompt.end():]
-            else:
-                if curcode:
-                    for item in do_insertions(insertions,
-                                              pylexer.get_tokens_unprocessed(curcode)):
-                        yield item
-                        curcode = ''
-                        insertions = []
-                yield match.start(), Generic.Output, line
-        if curcode:
-            for item in do_insertions(insertions,
-                                      pylexer.get_tokens_unprocessed(curcode)):
-                yield item
-
-#-----------------------------------------------------------------------------
-# Register the extension as a valid pygments lexer
-highlighting.lexers['ipython'] = IPythonConsoleLexer()
